d29e2a1758e752166692e40ecef0938b7a80df9d
Will M <willmcb234@gmail.com>
2020-10-05 12:38:40 -0400

    Latest Update

--- MafiaSimulator/Assets/Scripts/CardScripts/CurrencyDestruction.cs
+++ MafiaSimulator/Assets/Scripts/CardScripts/CurrencyDestruction.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class CurrencyDestruction : MonoBehaviour
 {
+    private TimePasses timePasses;
     public bool inCurrencyList;
+    private void Awake()
+    {
+        timePasses = GameObject.Find("TimePasses").GetComponent<TimePasses>();
+    }
     private void Update()
     {
         if(!inCurrencyList)
         {
+            timePasses.cardInSnap = false;
             Destroy(gameObject);
         }
     }
--- MafiaSimulator/Assets/Scripts/VerbScripts/VerbScripts/TimePasses.cs
+++ MafiaSimulator/Assets/Scripts/VerbScripts/VerbScripts/TimePasses.cs
@@ -8,45 +8,50 @@ public class TimePasses : MonoBehaviour
     private VerbAutoTimer AutoTimer;
     private CardPositioning timePasses, VerbA, VerbE, VerbI, VerbS;
     private CurrencyList currentCurrency;
-    private CardPositioning cardPos;
     public CardTimer c_timer;
     public bool cardInSnap;
     private void Awake()
     {
-        timePasses = GameObject.Find("TimePasses").GetComponent<CardPositioning>();
+        timePasses = GetComponent<CardPositioning>();
         VerbA = GameObject.Find("Verb_Action").GetComponent<CardPositioning>();
         VerbE = GameObject.Find("Verb_Explore").GetComponent<CardPositioning>();
         VerbI = GameObject.Find("Verb_Investigate").GetComponent<CardPositioning>();
         VerbS = GameObject.Find("Verb_Speak").GetComponent<CardPositioning>();
         AutoTimer = GetComponent<VerbAutoTimer>();
-        cardPos = GetComponent<CardPositioning>();
         currentCurrency = GameObject.Find("NarrativeController").GetComponent<CurrencyList>();
     }
     void Update()
     {
         foreach (GameObject card in currentCurrency.Currency)
         {
-            if(AutoTimer.timeLeft <= 0 && cardInSnap == true)
+            if(AutoTimer.timeLeft <= 0 && cardInSnap == true && card.GetComponent<CardTimer>().in_tSnap == true)
             {
                     card.GetComponent<CurrencyDestruction>().inCurrencyList = false;
-                    currentCurrency.Currency.RemoveRange(0, 1);
-                    timePasses.Card.RemoveRange(1, 1);
-                    VerbA.Card.RemoveRange(1, 1);
-                    VerbE.Card.RemoveRange(1, 1);
-                    VerbI.Card.RemoveRange(1, 1);
-                    VerbS.Card.RemoveRange(1, 1);
+                    removeFromLists();
                     cardInSnap = false;
             }
+
         }
         if (c_timer.in_tSnap == true)
         {
             cardInSnap = true;
         }
-        else
+        if(c_timer.in_tSnap == false)
+        {
             cardInSnap = false;
+        }
         if (!cardInSnap)
         {
             currentCurrency.Currency[0].gameObject.transform.position = timePasses.SnapPosition;
         }  
+    }
+    public void removeFromLists()
+    {
+        currentCurrency.Currency.RemoveRange(0, 1);
+        timePasses.Card.RemoveRange(1, 1);
+        VerbA.Card.RemoveRange(1, 1);
+        VerbE.Card.RemoveRange(1, 1);
+        VerbI.Card.RemoveRange(1, 1);
+        VerbS.Card.RemoveRange(1, 1);
     }
 }
